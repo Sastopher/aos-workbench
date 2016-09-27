900394e89fd9eface907d358796f8a9eeb894e78
Chris Rollock <sastopher@gmail.com>
2016-09-27 01:13:20 -0400

    More sensibile directory structure

---
+++ client/src/components/app.jsx
@@ -0,0 +1,14 @@
+// const React = require('react');
+// const Army = require('./army');
+const armyData = require('../vendor/2016-09-25/gwPoolPoints.csv');
+
+armyData.map(({ name, points }) => {
+
+})
+// class App extends React.Component {
+//   render() {
+//     return (<Army data={ armyData } />);
+//   }
+// }
+
+// module.exports = App;
