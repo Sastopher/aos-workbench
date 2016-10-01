c6b9633d311748a1e4780dc26cd343eeee1f6fec
Chris Rollock <sastopher@gmail.com>
2016-10-01 00:41:01 -0400

    Finish importing the rest of the army files

--- client/vendor/2016-09-25/index.js
+++ client/vendor/2016-09-25/index.js
@@ -5,6 +5,25 @@ const bretonnia = require('./bretonnia');
 const daemonsofchaos = require('./daemonsofchaos');
 const darkelves = require('./darkelves');
 const dwarfs = require('./dwarfs');
+const empire = require('./empire');
+const flesheatercourts = require('./flesheatercourts');
+const grots = require('./grots');
+const highelves = require('./highelves');
+const ironjawz = require('./ironjawz');
+const khornebloodbound = require('./khornebloodbound');
+const legionofazgorh = require('./legionofazgorh');
+const lizardmen = require('./lizardmen');
+const monstrousarcanum = require('./monstrousarcanum');
+const ogrekingdoms = require('./ogrekingdoms');
+const orruks = require('./orruks');
+const skaven = require('./skaven');
+const stormcasteternals = require('./stormcasteternals');
+const sylvaneth = require('./sylvaneth');
+const tamurkhanshorde = require('./tamurkhanshorde');
+const tomekings = require('./tomekings');
+const vampirecounts = require('./vampirecounts');
+const warriorsofchaos = require('./warriorsofchaos');
+const woodelves = require('./woodelves');
 
 
 module.exports = {
@@ -14,5 +33,24 @@ module.exports = {
   bretonnia,
   daemonsofchaos,
   darkelves,
-  dwarfs
+  dwarfs,
+  empire,
+  flesheatercourts,
+  grots,
+  highelves,
+  ironjawz,
+  khornebloodbound,
+  legionofazgorh,
+  lizardmen,
+  monstrousarcanum,
+  ogrekingdoms,
+  orruks,
+  skaven,
+  stormcasteternals,
+  sylvaneth,
+  tamurkhanshorde,
+  tomekings,
+  vampirecounts,
+  warriorsofchaos,
+  woodelves
 };
